af6b6bb2b2139bcee4b19b71fe0aa1b409e28106
z-taylcr7 <554795808@qq.com>
2022-06-12 15:54:40 +0800

    va

--- BPlusTree.hpp
+++ BPlusTree.hpp
@@ -13,7 +13,7 @@ namespace Geneva{
 //    (4096 - 5 * sizeof(int) - sizeof(bool)) / (sizeof(String)+sizeof(long long)+ sizeof(int)) - 1,
             int L = 100,
             //          (4096 - 4 * sizeof(int)) / (sizeof(String)+sizeof(long long)+ sizeof(int)) - 1,
-             int CACHESIZE = 50>
+             int CACHESIZE = 5>
     class BPlusTree {
         ///Announcement:
         ///go left: <key
