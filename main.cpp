3f829c1df04c94e788d79a7b15f1a66cf075cacc
z-taylcr7 <554795808@qq.com>
2022-06-12 15:32:42 +0800

    storage io update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,77 @@
 #include <iostream>
 #include "BPlusTree.hpp"
-#include "ztest.hpp"
-//
+//#include "ztest.hpp"
+
 #include <algorithm>
 using namespace Geneva;
-BPlusTree<50,50,250>Q("test");
+BPlusTree<>Q("test");
+void test(){
+    String str("chaokawaii");
+    String key=str;
+    for(int i=1;i<=400;i++) {
+        Q.insert(std::make_pair(str, i), i);
+    }
+    Q.show();
+    for(int i=400;i>0;i-=2){
+        Q.remove(std::make_pair(str,i));
+    }
+    Q.show();
+    std::vector<std::pair<int,long long>>pat;
+    Q.find(key,pat);
+    if(pat.empty())std::cout<<"null";
+    for(std::pair<int,long long> & i : pat){
+        if(i.second%2==0)std::cout<<std::endl;
+        std::cout<<i.second<<' ';
+    }
+    std::cout<<std::endl;
 
+}
+
+void show20(){
+    String ts("Tay");
+    for(int i=1;i<20;i++)Q.insert(std::make_pair(ts,i),i);
+    Q.show();
+}
+void test2(int x){
+    String str("chao");
+    String str2("chaochao");
+    int max=x;
+    for(int i=1;i<=2*max;i++){
+        Q.insert(std::make_pair(str,i),i);
+    }
+    for(int i=4*max;i>2*max;i--){
+        Q.insert(std::make_pair(str2,i),i);
+    }
+    String key=str;
+    for(int i=2*max-1;i>0;i-=2){
+        Q.remove(std::make_pair(str,i));
+    }
+    for(int i=2*max+1;i<4*max;i+=2){
+
+        Q.remove(std::make_pair(str2,i));
+
+    }
+    std::vector<std::pair<int,long long>>pat;
+    Q.find(key,pat);
+    if(pat.empty())std::cout<<"null";
+    for(std::pair<int,long long> & i : pat){
+        if(i.second%2==1)std::cout<<i.second<<std::endl;
+    }
+    key=str2;
+    pat.clear();
+    Q.find(key,pat);
+    if(pat.empty())std::cout<<"null";
+    for(std::pair<int,long long> & i : pat){
+        if(i.second%2==1)std::cout<<i.second<<std::endl;
+    }
+    std::cout<<"pass"<<' ';
+}
 int main() {
+
+
     int n;std::cin>>n;
+   // if(n==-1){test();test2(250);std::cin>>n;}
+
     while(n--){
         std::string command;
         std::cin>>command;
@@ -22,7 +86,7 @@ int main() {
             String key;
             int value;
             std::cin>>key>>value;
-            Q.erase(std::make_pair(key,value));
+            Q.remove(std::make_pair(key,value));
         }
         if(command=="find"){
             String key;
@@ -35,6 +99,6 @@ int main() {
             }
             std::cout<<std::endl;
         }
+        if(command=="quit")break;
     }
-
 }
--- ztest.hpp
+++ ztest.hpp
@@ -7,23 +7,22 @@
 #include <vector>
 namespace Geneva{
     void test(){
-        BPlusTree<50,50,250> Q("test");
-        String str("chao");
+        BPlusTree<> Q("test");
+        String str("chaochao");
         String key=str;
         for(int i=1;i<=400;i++) {
             Q.insert(std::make_pair(str, i), i);
         }
         Q.show();
         for(int i=400;i>0;i-=2){
-            Q.erase(std::make_pair(str,i));
+            Q.remove(std::make_pair(str,i));
         }
         Q.show();
         std::vector<std::pair<int,long long>>pat;
         Q.find(key,pat);
         if(pat.empty())std::cout<<"null";
         for(std::pair<int,long long> & i : pat){
-            if(i.second%2==0)        std::cout<<std::endl;
-
+            if(i.second%2==0)std::cout<<std::endl;
             std::cout<<i.second<<' ';
         }
         std::cout<<std::endl;
@@ -40,6 +39,7 @@ namespace Geneva{
         BPlusTree<> Q("test");
 
         String str("chao");
+        String str2("chaochoa");
         int max=x;
         for(int i=1;i<=2*max;i++){
             Q.insert(std::make_pair(str,i),i);
@@ -53,21 +53,20 @@ namespace Geneva{
         String key=str;
 
         for(int i=2*max-1;i>0;i-=2){
-            Q.erase(std::make_pair(str,i));
+            Q.remove(std::make_pair(str,i));
         }
         for(int i=2*max+1;i<4*max;i+=2){
 
-            Q.erase(std::make_pair(str,i));
+            Q.remove(std::make_pair(str,i));
 
         }
         std::vector<std::pair<int,long long>>pat;
         Q.find(key,pat);
         if(pat.empty())std::cout<<"null";
         for(std::pair<int,long long> & i : pat){
-            if(i.second%2==1)std::cout<<std::endl;
-            std::cout<<i.second<<' ';
+            if(i.second%2==1)std::cout<<i.second<<std::endl;
         }
-        std::cout<<std::endl;
+        std::cout<<"pass"<<' ';
     }
 }
 #endif //BPLUSTREE_ZTEST_HPP
